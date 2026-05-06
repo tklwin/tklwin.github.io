ad61efb659fb96f89012c4de4e011ae541e60b82
tklwin <150787051+tklwin@users.noreply.github.com>
2026-05-06 08:08:07 +0000

    Deploying to gh-pages from @ tklwin/tklwin.github.io@7cdf1066740fb28b4a33aa5106c25f3f7332d4d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-install-microsoft-sql-server-on-mac-m2-air-using-docker-and-azure-data-studio",
         
           title: "Install Microsoft SQL Server on Mac M2 Air using Docker and Azure Data...",
